601221855e85cda5d4c6cafb4e52befb9283d3e9
S <ap311036@gmail.com>
2018-08-08 11:33:03 +0800

    Updates

--- ajax.php
+++ ajax.php
@@ -2,9 +2,9 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 //獲取回調函數名
-$jsoncallback = htmlspecialchars($_REQUEST ['KeyWord']);
+$jsoncallback = htmlspecialchars($_REQUEST ['keyWord']);
 //json資料
-$file = file_get_contents('https://uhotel.liontravel.com/search/keyword?'.$jsoncallback);
+$file = file_get_contents('https://uhotel.liontravel.com/search/keyWord=?'.$jsoncallback);
 //輸出jsonp格式的資料
 echo $file;
 ?>
